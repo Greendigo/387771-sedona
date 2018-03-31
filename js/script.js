e071859655d4fd97c4a1e0418a3d97fb884b92c5
Greendigo <gregorytsymbal@gmail.com>
2018-03-31 16:28:13 +0300

    + JS, оживление страницы

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var formSwitch = document.querySelector(".housing-search-switch");
+var form = document.querySelector(".housing-search-form");
+var fieldDateArrival = form.querySelector("input[name=date-arrival]");
+var fieldDateDeparture = form.querySelector("input[name=date-departure]");
+
+var isStorageSupported = true;
+var dateArrival = "";
+var dateDeparture = "";
+
+try {
+  dateArrival = localStorage.getItem("dateArrival");
+  dateDeparture = localStorage.getItem("dateDeparture");
+} catch (err) {
+  isStorageSupported = false;
+}
+
+formSwitch.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  form.classList.toggle("housing-search-form-hidden");
+  if (!form.classList.contains("housing-search-form-hidden")) {
+    fieldDateArrival.focus();
+
+    if (dateArrival) {
+      fieldDateArrival.value = dateArrival;
+    }
+    if (dateDeparture) {
+      fieldDateDeparture.value = dateDeparture;
+    }
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (isStorageSupported) {
+    if (fieldDateArrival.value) {
+      localStorage.setItem("dateArrival", fieldDateArrival.value);
+    }
+    if (fieldDateDeparture.value) {
+      localStorage.setItem("dateDeparture", fieldDateDeparture.value);
+    }
+  }
+});
